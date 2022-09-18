9e44a054ec23963565990c743f07274b1736621c
leerivera <leerivera80@gmail.com>
2022-09-18 15:18:55 -0400

    more

--- s.js
+++ s.js
@@ -9,3 +9,12 @@
 function chromosomeCheck(sperm) {
     return sperm === 'XY' ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter.";
 }
+
+function chromosomeCheck(sperm) {
+    if(sperm == "XY")
+    {
+      return "Congratulations! You're going to have a son.";
+    }
+    
+    return "Congratulations! You're going to have a daughter.";
+    }
