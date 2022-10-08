4f7e73aed4d56b8e90984dd75fc5dde5323f9e29
leerivera <leerivera80@gmail.com>
2022-10-08 19:25:05 -0400

    more

--- count.js
+++ count.js
@@ -35,5 +35,12 @@ function count(string) {
     return charCount;
   }
   
+  function count (string) {  
+    var count = {};
+    string.split('').forEach(function(s) {
+       count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
+  }
   
   
