365e6a4b1de189840fe3ceb25cd5e4322410397c
leerivera <leerivera80@gmail.com>
2022-10-21 14:39:17 -0400

    more

--- wolf.js
+++ wolf.js
@@ -12,3 +12,13 @@ function warnTheSheep(queue) {
     }
   }
   
+    function warnTheSheep(queue) {
+        const wolfIndex = queue.findIndex((animal) => animal === "wolf");
+        const sheepIndex = queue.length - wolfIndex - 1;
+        
+        if (wolfIndex === queue.length - 1) {
+            return "Pls go away and stop eating my sheep";
+        } else {
+            return `Oi! Sheep number ${sheepIndex}! You are about to be eaten by a wolf!`;
+        }
+        }
