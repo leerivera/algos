732d7840955761cd0c04d95f58baa84b4b70a4b3
leerivera <leerivera80@gmail.com>
2022-10-27 22:23:59 -0400

    more

--- wolf.js
+++ wolf.js
@@ -31,3 +31,8 @@ function warnTheSheep(queue) {
           function warnTheSheep(q) {
             return q[q.length-1] === 'wolf' ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${q.length - (q.indexOf('wolf')+1)}! You are about to be eaten by a wolf!`
           }
+
+          let warnTheSheep = q => {
+            let index = q.reverse().indexOf('wolf');
+            return index ? `Oi! Sheep number ${index}! You are about to be eaten by a wolf!` : 'Pls go away and stop eating my sheep';
+          }
