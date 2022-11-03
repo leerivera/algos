6558324388b709f83a6f34291bbe6d42a9ca56c4
leerivera <leerivera80@gmail.com>
2022-11-02 22:36:30 -0400

    more

--- bin.js
+++ bin.js
@@ -3,3 +3,5 @@ function binToDec(bin){
   }
 
   const binToDec = bin => [...bin].reduce((dec, bit) => dec << 1 | bit, 0);
+
+  const binToDec = bin => [...bin].reverse().reduce((acc, elt, idx) => acc + elt * 2 ** idx, 0)
