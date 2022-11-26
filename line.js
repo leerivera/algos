68cccd1a7e6f0c0f6ddba35a5ea4b7b9da8876af
leerivera <leerivera80@gmail.com>
2022-11-26 13:35:17 -0500

    more lines

--- line.js
+++ line.js
@@ -7,7 +7,15 @@
 // output
 
 // The function should return an integer, the total time required.
-
+ 
+function queueTime(customers, n) {
+    let tills = Array(n).fill(0); // Initialize tills with 0 time
+    for (let i = 0; i < customers.length; i++) {
+        let idx = tills.indexOf(Math.min(...tills)); // Find the till with minimum time
+        tills[idx] += customers[i]; // Add the customer to that till
+    }
+    return Math.max(...tills); // The total time is the maximum time of any till
+}
 function queueTime(customers, n) {
     let tills = Array(n).fill(0); // Initialize tills with 0 time
 
