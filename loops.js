ee1854574fd0935265c19aff23898434a69cc146
leerivera <leerivera80@gmail.com>
2022-12-08 21:29:37 -0500

    a

--- loops.js
+++ loops.js
@@ -12,3 +12,8 @@ for (let i = 0; i < 10; i++) {
     console.log(`${key}: ${person[key]}`);
   }
   
+
+  const numbers = [1, 2, 3, 4, 5];
+for (const number of numbers) {
+  console.log(number);
+}
