369d0e0d52787dc854a9f09c83c974bccbe3fb5d
leerivera <leerivera80@gmail.com>
2022-12-08 21:35:16 -0500

    comments

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
+// for: This loop is used to execute a block of code a specific number of times. For example:
 for (let i = 0; i < 10; i++) {
     console.log(i);
   }
+
+//   for...in: This loop is used to iterate over the enumerable properties of an object. For example:
   
 
   const person = {
@@ -11,19 +14,21 @@ for (let i = 0; i < 10; i++) {
   for (const key in person) {
     console.log(`${key}: ${person[key]}`);
   }
-  
+//   for...of: This loop is used to iterate over the values of an iterable object, such as an array. For example:
 
   const numbers = [1, 2, 3, 4, 5];
 for (const number of numbers) {
   console.log(number);
 }
+// while: This loop is used to execute a block of code repeatedly while a specified condition is met. For example:
+
 
 let i = 0;
 while (i < 10) {
   console.log(i);
   i++;
 }
-
+// do...while: This loop is similar to the while loop, but it guarantees that the code inside the loop will be executed at least once. For example:
 let i = 0;
 do {
   console.log(i);
