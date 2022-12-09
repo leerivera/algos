da4ea913062d23064d54f7abc91f05b722084068
leerivera <leerivera80@gmail.com>
2022-12-08 21:28:52 -0500

    another4

--- loops.js
+++ loops.js
@@ -2,3 +2,13 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
   }
   
+
+  const person = {
+    name: 'John Doe',
+    age: 32,
+    city: 'New York'
+  };
+  for (const key in person) {
+    console.log(`${key}: ${person[key]}`);
+  }
+  
