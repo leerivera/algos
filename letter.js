f9eba354edcc630aef42e4ccea9e392fd6ba33f8
leerivera <leerivera80@gmail.com>
2022-12-24 13:43:03 -0500

    more lines

--- letter.js
+++ letter.js
@@ -1,3 +1,10 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
 function position(letter) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     const positionInAlphabet = alphabet.indexOf(letter.toLowerCase()) + 1;
