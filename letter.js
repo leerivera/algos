a7ab28f2cbb8008d757e25e056f508f6766e5d80
leerivera <leerivera80@gmail.com>
2022-12-30 13:46:29 -0500

    more lines

--- letter.js
+++ letter.js
@@ -16,4 +16,16 @@ function position(letter) {
     const positionInAlphabet = letter.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0) + 1;
     return `Position of alphabet: ${positionInAlphabet}`;
   }
+
+  function position(letter) {
+    const alphabetPositions = {
+      'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7,
+      'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+      'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21,
+      'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+    };
+  
+    return `Position of alphabet: ${alphabetPositions[letter.toLowerCase()]}`;
+  }
+  
   
