4b1eafa74a2e9eeb8b5f048e1ad8492aa1d3d467
leerivera <leerivera80@gmail.com>
2023-01-11 19:11:43 -0500

    more lines

---
+++ enough.js
@@ -0,0 +1,8 @@
+function enough(cap, on, wait) {
+    // Calculate the total passengers on the bus including those waiting
+    let total = on + wait;
+
+    // If total passengers is less than or equal to the bus capacity, return 0
+    // Otherwise, return the difference between total passengers and bus capacity
+    return total <= cap ? 0 : total - cap;
+}
